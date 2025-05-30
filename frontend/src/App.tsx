5d35d72e7e026039a29437d9daf7b3dfb6ee444d
sirserg <sirserg05@gmail.com>
2025-05-30 16:40:21 +0300

    feat: add task solution

---
+++ frontend/src/App.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState, type FormEvent } from "react";
+import "./App.css";
+import { useSocket } from "./hooks/use-socket.hook";
+import { Rooms } from "./components/rooms/Rooms";
+
+function App() {
+  const [username, setUsername] = useState("");
+
+  const socketRef = useSocket();
+
+  useEffect(() => {
+    const socket = socketRef.current;
+    if (!socket) return;
+
+    localStorage.setItem("username", "guest");
+
+    socket.on("username", (data) => {
+      localStorage.setItem("username", data);
+    });
+
+    return () => {
+      socket.off("username");
+    };
+  }, [socketRef]);
+
+  const handleChangeUsername = (event: FormEvent) => {
+    event.preventDefault();
+    const socket = socketRef.current;
+    if (socket && username.trim() !== "") {
+      socket.emit("set-username", username);
+      localStorage.setItem("username", username);
+    }
+  };
+
+  return (
+    <div>
+      <p>{`Your username: ${localStorage.getItem("username")}`}</p>
+      <form onSubmit={handleChangeUsername}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <button type="submit">Change username</button>
+      </form>
+      <Rooms />
+    </div>
+  );
+}
+
+export default App;
